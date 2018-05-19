d0940c89f1d3d2985b15b8ca503b846c5c9ab0ae
Hong Wei <teohongwei898@gmail.com>
2018-05-19 11:29:33 +0800

    Create bugs.c

---
+++ FE1008-Computing/bugs.c
@@ -0,0 +1,10 @@
+/* Calculate and display the difference of two input values /*
+include <stdio.h>
+int Main(void)
+{
+int X, x, sum;
+scanf("%d", & X);
+scanf("%f", & x);
+X + x = sum;
+printf("The sum of %d and %d is %d\n", X, x, sum);
+return 0;
